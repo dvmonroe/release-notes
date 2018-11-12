29e4c00ae94b76e1855b6d5cc45c265ab81ccc47
Andrew Mason <andrew.mason@n2pub.com>
2018-11-12 17:59:19 -0500

    start adding link tests

--- spec/release/notes/link_spec.rb
+++ spec/release/notes/link_spec.rb
@@ -3,4 +3,27 @@
 require "spec_helper"
 
 describe Release::Notes::Link do
+  class LinkTestClass
+    include Release::Notes::Link
+    attr_reader :config
+
+    def initialize(config)
+      @config = config
+    end
+  end
+
+  describe "#log" do
+    let(:klass) { LinkTestClass }
+    let(:config) { Release::Notes.configuration }
+    let(:lines) do
+
+    end
+    subject { klass.new(config) }
+
+    context "#link_lines" do
+      it "splits the lines" do
+        expect(subject.link_lines(lines: lines)).to eq lines.split(/\s/)
+      end
+    end
+  end
 end
