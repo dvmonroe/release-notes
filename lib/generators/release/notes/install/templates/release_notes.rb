ec367264a54dc1b6ee432eb2a8500d5831ae6880
Andrew Mason <andrew.mason@n2pub.com>
2018-11-26 17:39:52 -0500

    fix issue with including links

--- lib/generators/release/notes/install/templates/release_notes.rb
+++ lib/generators/release/notes/install/templates/release_notes.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 Release::Notes.configure do |config|
-  # config.output_file = './RELEASE_NOTES.md'
-  # config.temp_file = './release-notes.tmp.md'
+  # config.output_file = "./RELEASE_NOTES.md"
+  # config.temp_file = "./release-notes.tmp.md"
   # config.include_merges = false
   # config.ignore_case = true
-  # config.log_format = '- %s'
+  # config.log_format = "- %s"
   # config.extended_regex = true
   # config.bug_labels = %w(Fix Update)
   # config.feature_labels = %w(Add Create)
   # config.misc_labels = %w(Refactor)
-  # config.bug_title = '**Fixed bugs:**'
-  # config.feature_title = '**Implemented enhancements:**'
-  # config.misc_title = '**Miscellaneous:**'
-  # config.log_all_title = '**Other:**'
+  # config.bug_title = "**Fixed bugs:**"
+  # config.feature_title = "**Implemented enhancements:**"
+  # config.misc_title = "**Miscellaneous:**"
+  # config.log_all_title = "**Other:**"
   # config.log_all = false
   # config.link_to_labels = %w()
   # config.link_to_humanize = %w()
   # config.link_to_sites = %w()
   # config.force_rewrite = false
-  # config.timezone = 'America/New_York'
+  # config.timezone = "America/New_York"
 end
--- lib/release/notes/link.rb
+++ lib/release/notes/link.rb
@@ -9,44 +9,35 @@ module Release
         delegate :link_to_labels, :link_to_sites, :link_to_humanize, to: :config
 
         def link_lines(lines:)
-          @new_lines = ""
-          split_lines(lines)
-          @new_lines
+          new_lines = ""
+          lines.split("\n").each do |line|
+            unless link_to_labels.any? { |la| line.include? la }
+              new_lines += "#{line}\n"
+              next
+            end
+            link_to_labels.each_with_index do |label, i|
+              next unless line.include? label
+
+              words = line.split(/\s/)
+              words.each do |word|
+                next unless (word =~ /^#.*/)&.zero?
+
+                new_lines += "#{replace(line, word, label, i)}\n"
+              end
+            end
+          end
+          new_lines
         end
 
         # :nocov:
         private
 
         # @api private
-        def split_lines(lines)
-          lines.split("\n").each do |line|
-            unless link_to_labels { |la| line.include? la }
-              @new_lines += "#{line}\n"
-              next
-            end
-            split_words(line)
-          end
-        end
-
-        # @api private
-        def split_words(line)
-          link_to_labels.each_with_index do |label, i|
-            next unless line.include? label
-
-            words = line.split(/\s/)
-            words.each do |word|
-              next unless (word =~ /^#.*/)&.zero?
-
-              @new_lines += "#{replace(line, word, label, i)}\n"
-            end
-          end
-        end
-
-        # @api private
         def replace(line, issue_number, label, index)
           identifier = "#{label.split(/\s/)[0]} #{issue_number}"
           humanized = "#{link_to_humanize[index]} #{issue_number}"
-          linked = "[#{humanized}](#{link_to_sites[index]}\/#{issue_number.tr('^0-9', '')})"
+          linked = "[#{humanized}](#{link_to_sites[index]})"
+
           line.gsub! identifier, linked
           line
         end
